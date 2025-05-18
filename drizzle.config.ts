ca1d826e49c62389ac9ffc1fce59da74fe089d94
vineet kumar <vineetkumar12392@gmail.com>
2025-05-18 23:00:24 +0530

    Add files via upload

---
+++ drizzle.config.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'; // this automatically loads .env
+import type { Config } from 'drizzle-kit';
+
+// export default {
+const config = {
+  schema: './drizzle/schema.ts',
+  out: './drizzle/migrations',
+  dialect:'postgresql',
+  // dialect: 'pg',    
+  // driver: 'pg',
+  dbCredentials: {
+    // connectionString: 'postgresql://postgres@localhost:5432/urlmonitoring',
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'no-password', // fill this if your DB has a password
+    database: 'urlmonitoring',
+  },
+}satisfies Config;
+
+export default config;
